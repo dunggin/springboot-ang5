26b66d59072350dc2bc5bf26fabf840be37c36d0
dunggin <dungho451@gmail.com>
2018-08-12 09:40:29 +0700

    master: add lombok

--- backend/src/main/java/angular5_springboot/entity/Student.java
+++ backend/src/main/java/angular5_springboot/entity/Student.java
@@ -5,7 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "student")
 public class Student {
 
@@ -21,48 +30,5 @@ public class Student {
 
 	@Column(name = "birthday")
 	String birthday;
-
-	public Student() {
-	}
-
-	public Student(String student_id, String student_name, String classroom, String birthday) {
-		super();
-		this.student_id = student_id;
-		this.student_name = student_name;
-		this.classroom = classroom;
-		this.birthday = birthday;
-	}
-
-	public String getStudent_id() {
-		return student_id;
-	}
-
-	public void setStudent_id(String student_id) {
-		this.student_id = student_id;
-	}
-
-	public String getStudent_name() {
-		return student_name;
-	}
-
-	public void setStudent_name(String student_name) {
-		this.student_name = student_name;
-	}
-
-	public String getClassroom() {
-		return classroom;
-	}
-
-	public void setClassroom(String classroom) {
-		this.classroom = classroom;
-	}
-
-	public String getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
 
 }
